5c7918c847320b3355b9a25e5448d35017dfc2f3
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2023-05-11 13:22:55 -0300

    feat: add slogan highlight component

--- src/components/landing/highlight/index.tsx
+++ src/components/landing/highlight/index.tsx
@@ -1,5 +1,12 @@
 import Area from "../common/area";
+import Slogan from "./slogan";
 
 export default function Highlight() {
-  return <Area>Highlight</Area>;
+  return (
+    <Area id="home" className="pt-20">
+      <div className="h-[500px] flex items-center">
+        <Slogan />
+      </div>
+    </Area>
+  );
 }
