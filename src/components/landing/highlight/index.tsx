e735ce819f355a3bc3cba88a1a3a07f36c4e6232
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2023-05-11 13:39:24 -0300

    feat: add responsive image common component

--- src/components/landing/highlight/index.tsx
+++ src/components/landing/highlight/index.tsx
@@ -1,11 +1,15 @@
+import main from "../../../../public/main.jpg";
+
 import Area from "../common/area";
+import ResponsiveImage from "../common/responsive-image";
 import Slogan from "./slogan";
 
 export default function Highlight() {
   return (
     <Area id="home" className="pt-20">
-      <div className="h-[500px] flex items-center">
+      <div className="h-[500px] flex items-center justify-around">
         <Slogan />
+        <ResponsiveImage image={main} className="rotate-3 hidden md:inline" />
       </div>
     </Area>
   );
