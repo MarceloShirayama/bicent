2aad11c77204c9ac7be0a0331b3aa3a907e72905
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2023-05-11 08:20:49 -0300

    feat: add landing component

---
+++ src/components/landing/index.tsx
@@ -0,0 +1,18 @@
+import Page from "../template/page";
+import Advantages from "./advantages";
+import Footer from "./footer";
+import Header from "./header";
+import Highlight from "./highlight";
+import Reviews from "./reviews";
+
+export default function Landing() {
+  return (
+    <Page>
+      <Header />
+      <Highlight />
+      <Advantages />
+      <Reviews />
+      <Footer />
+    </Page>
+  );
+}
