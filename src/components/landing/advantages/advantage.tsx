504d8214e91041480e873dbac736fdf8310f45a6
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2023-05-14 10:34:29 -0300

    feat: add advantages landing component

---
+++ src/components/landing/advantages/advantage.tsx
@@ -0,0 +1,35 @@
+import ResponsiveImage from "../common/responsive-image"
+
+type Props = {
+    image: any
+    title: string
+    subtitle: string
+    invert?: boolean
+}
+
+export default function Advantage(props: Props) {
+    return (
+        <div className={`
+            flex flex-col justify-around items-center w-full gap-6
+            ${props.invert ? 'sm:flex-row-reverse' : 'sm-flex-row'}
+        `}>
+            <ResponsiveImage
+                image={props.image}
+                className={props.invert ? 'sm-rotate-6' : 'sm-rotate-6'}
+            />
+            <div className="
+                flex flex-col gap-y-6 sm:w-[350px
+                text-center sm:text-left
+            ">
+                <div className="
+                    flex flex-col text-white
+                    font-black text-2xl sm:text-4xl
+                ">
+                    {props.title}
+                </div>
+                <span className="font-light text-base sm:text-lg text-zinc-500"
+                >{props.subtitle}</span>
+            </div>
+        </div>
+    )
+}
