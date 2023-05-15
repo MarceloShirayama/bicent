ee3b23e834624a91c14bb5325923628c8e4938fd
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2023-05-15 08:17:19 -0300

    feat: add id common core

--- src/data/constants/fakeUser.ts
+++ src/data/constants/fakeUser.ts
@@ -1,7 +1,8 @@
+import { Id } from "@/logic/core/common/id";
 import { User } from "@/logic/core/user/types";
 
 export const fakeUser: User = {
-    id: '1',
+    id: Id.new(),
     name: 'João Da Silva',
     email: 'joao@mail.com',
     imageUrl: null
--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -6,7 +6,7 @@ export default function UserRegister() {
         <Page>
             <Header />
             <div>
-                Cadastro de Usuário
+                Usuário!!!
             </div>
         </Page>
     )
